a0330274c3a88963750abab84fc160cfe3f1ca85
yhoungdev <yhoungdev@gmail.com>
2023-05-02 22:53:05 +0100

    feat: update

--- src/data/trending.ts
+++ src/data/trending.ts
@@ -15,6 +15,16 @@ export const trending = [
         aritstName: 'Carter Doggy Black',
         cover: 'https://res.cloudinary.com/dhdqt4xwu/image/upload/v1683063715/gurumusic/photo_5949595922457541146_y_glsbwu.jpg'
     },
+    {
+        title: 'Pull it down' ,
+        aritstName: 'Nowas Gold',
+        cover: 'https://res.cloudinary.com/dhdqt4xwu/image/upload/v1683064222/gurumusic/photo_5949595922457541156_y_gpepqv.jpg'
+    },
+    {
+        title: 'Teaser' ,
+        aritstName: 'Swaggu Moni',
+        cover: 'https://res.cloudinary.com/dhdqt4xwu/image/upload/v1683064220/gurumusic/photo_5949595922457541158_y_ujuoub.jpg'
+    },
   
     
 ]
